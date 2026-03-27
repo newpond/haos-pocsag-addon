c1a01f4783fb864fd07decf862d0afb84b183a17
newpond <92895074+newpond@users.noreply.github.com>
2026-03-27 17:51:40 +0000

    Update run.sh

--- pocsag_decoder/run.sh
+++ pocsag_decoder/run.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/with-contenv bash
 
-FREQ=$(jq --raw-output ".frequency" /data/options.json)
-GAIN=$(jq --raw-output ".gain" /data/options.json)
-MQTT=$(jq --raw-output ".mqtt_host" /data/options.json)
-TOPIC=$(jq --raw-output ".mqtt_topic" /data/options.json)
-BAUD=$(jq --raw-output ".baud" /data/options.json)
-
-echo "Starting POCSAG decoder..."
-echo "Frequency: ${FREQ} MHz | Gain: ${GAIN} | Baud: ${BAUD}"
+FREQ=$(jq -r ".frequency" /data/options.json)
+GAIN=$(jq -r ".gain" /data/options.json)
+TOPIC=$(jq -r ".mqtt_topic" /data/options.json)
+BAUD=$(jq -r ".baud" /data/options.json)
 
 rtl_fm -f ${FREQ}M -M fm -s 22050 -g ${GAIN} | \
 multimon-ng -t raw -a ${BAUD} - | \
-python3 /parser.py "${MQTT}" "${TOPIC}"
+python3 /parser.py "${TOPIC}"
